b8d36e9d72ff95e13dfaa8d0571852dc6d388bf7
ehimedamikan <mikan.sup.all@gmail.com>
2020-02-24 12:22:37 +0900

    タスク登録用のフォームリクエストクラスを作成

---
+++ app/Http/Requests/TaskRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {   
+        // デフォルトはfalseなのでtrueにする
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:20'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => '入力必須です',
+            'title.max' => '20文字以内で入力してください',
+        ];
+    }
+}
