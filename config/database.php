1ac388e038f12530d6a465506c34252eca061ffe
ehimedamikan <mikan.sup.all@gmail.com>
2020-03-10 00:56:55 +0900

    カラムバイト制限を回避する用に設定

--- config/database.php
+++ config/database.php
@@ -52,8 +52,11 @@ return [
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', 'C:/xampp/mysql/mysql.sock'),
+            // カラムのバイト数制限を回避するためにcharasetを変更（サーバ側のMySQLのバージョンが低いことも原因）
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
+            // 'charset' => 'utf8',
+            // 'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
