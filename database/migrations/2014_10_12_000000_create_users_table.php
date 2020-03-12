7a7853d69d6e370dbccea5b4d791b1b93bd460c9
ehimedamikan <mikan.sup.all@gmail.com>
2020-03-13 00:33:31 +0900

    Twitter認証用のカラムを追加

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('twitter_id')->unique(); // add
             $table->string('name');
             $table->string('email',191)->unique();
             $table->timestamp('email_verified_at')->nullable();
