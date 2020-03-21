48d31eb136ccc9ea8f841cc84195cd6258b893b6
ehimedamikan <mikan.sup.all@gmail.com>
2020-03-21 22:54:10 +0900

    laravel MIX にbrowserSyncを導入

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,17 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/style.scss', 'public/css')
-    // コンパイルしたファイルのバージョニングを有効にする
+mix.js('resources/js/app.js', 'public/js') // jsのビルド（右が開発環境、左が実際に読み込むファイルの出力先）
+    .sass('resources/sass/style.scss', 'public/css') // SCSSのビルド（JSと同じフォルダ構成）
+    // version()で、コンパイルしたファイルのバージョニングを有効にする
     // ビルドするたびにコンパイルしたファイルの URL にランダムな文字列を付けてブラウザがキャッシュを読まないようにする
     .version()
-    .sourceMaps();
+    .sourceMaps()
+    .browserSync({
+        // https://browsersync.io/docs/options/#option-proxy
+        // proxyオプションで、ローカルホストのポートを指定
+        // laravelのビルドインサーバでのポートは8000番なのでそれと同じ値を指定
+        proxy: {
+            target: "http://localhost:8000",
+        },
+    }); 
