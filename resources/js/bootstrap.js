a0152954887a9d73a2d557107c6da82ef491de47
ehimedamikan <mikan.sup.all@gmail.com>
2020-03-30 00:23:24 +0900

    コメント等を削除

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,7 +23,7 @@ try {
 
 window.axios = require('axios');
 
-// デフォルトはこっちの記述、ヘッダーにトークン詰めてない？
+// POST時に、VerifyCsrfTokenミドルウェアがX-CSRF-TOKENリクエストヘッダもチェックします
 // https://readouble.com/laravel/5.8/ja/csrf.html#csrf-x-xsrf-token
 // Ajaxリクエストであることを示すヘッダーを付与する
 // ここをコメントアウトすると419エラーが出る（csrf用のトークンが無い）
@@ -31,19 +31,3 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo';
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     encrypted: true
-// });
