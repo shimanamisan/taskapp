b2a16b0dea3de451ebed2cdc55f0b14ca465ced6
shimanamisan <mikan.sup.all@gmail.com>
2020-09-03 22:00:03 +0900

    追加でコミット

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
     <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="/css/style.css">
-      <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-      <title>TaskApp</title>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-138230091-1"
+        ></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+
+        gtag("config", "UA-138230091-1");
+        </script>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="/css/style.css">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+        <title>TaskApp</title>
       
         <script>
             window.Laravel = {};
