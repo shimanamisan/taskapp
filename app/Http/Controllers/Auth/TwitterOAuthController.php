330e4ba38e33878beb21f272f6dfc75918844516
shimanamisan <mikan.sup.all@gmail.com>
2020-09-12 14:42:49 +0900

    Twitter認証処理を修正

--- app/Http/Controllers/Auth/TwitterOAuthController.php
+++ app/Http/Controllers/Auth/TwitterOAuthController.php
@@ -15,7 +15,7 @@ class TwitterOAuthController extends Controller
     public function redirectToTwitter(): JsonResponse // 返り値の方をJSON形式と宣言する PHP7から返り値に型宣言がかけるようになった
     {
         // リダイレクトURLをJSON形式で返す
-        $redirect_url = Socialite::driver('twitter')->redirect()->getTargetUrl();
+        $redirect_url =  Socialite::driver('twitter')->redirect()->getTargetUrl();
         \Log::debug('リダイレクトURLを取得しています。。。');
         \Log::debug('    ');
         return response()->json(['redirect_url' => $redirect_url]);
@@ -25,28 +25,41 @@ class TwitterOAuthController extends Controller
     {
         try {
             $socialUser = Socialite::driver('twitter')->user();
-    
-            // firstOrNew：第一引数にカラム名、第二引数に検索値を入れてデータベースを検索するメソッド
-            // このメソッドは保存するにはsave()メソッドを呼ぶ必要がある。（類似：firstOrCreate）
-            // https://readouble.com/laravel/5.8/ja/eloquent.html
-            $user = User::firstOrNew([
-                'email' => $socialUser->getEmail()
-            ]);
+
+            // メールアドレスが未登録の場合は、ユーザー登録していないユーザーと判定する
+            $userInfo = User::where('email', $socialUser->getEmail())->where('delete_flg', 0)->first();
+        
+            // 既にメールアドレスが登録されていて、パスワードが空でなければメールアドレスから登録されている旨を通知する
+            if (!empty($userInfo->email_register_flg)) {
+                return $this->errorResponse("Twitterアカウントではなくメールアドレスから登録されたユーザーです。ログインページよりログインしてください。");
+            }
     
             // exists：countメソッドの代わりに使用する。クエリで引っ張ってきたレコードが存在しているか判定する。（類似：doesntExist）
             // https://readouble.com/laravel/5.8/ja/queries.html
-            if ($user->exists) {
-                Auth::login($user);
-                return response()->json($user);
+            if (!empty($userInfo)) {
+                $userInfo->name = $socialUser->getNickname();
+                $userInfo->email = $socialUser->getEmail();
+                $userInfo->my_twitter_id = $socialUser->getId();
+                $userInfo->twitter_token = $socialUser->token;
+                $userInfo->twitter_token_secret = $socialUser->tokenSecret;
+                $userInfo->pic = $socialUser->getAvatar();
+                $userInfo->save();
+
+                Auth::login($userInfo);
+                return response()->json($userInfo);
             }
     
-            $user->name = $socialUser->getNickname();
-            $user->email = $socialUser->getEmail();
-            $user->twitter_token = $socialUser->token;
-            $user->twitter_token_secret = $socialUser->tokenSecret;
-            $user->pic = $socialUser->getAvatar();
-    
-            $user->save();
+            // 未登録ユーザーだったときの処理
+            $newUser = User::create([
+            'name' => $socialUser->getNickname(),
+            'email' => $socialUser->getEmail(),
+            'my_twitter_id' => $socialUser->getId(),
+            'twitter_token' => $socialUser->token,
+            'twitter_token_secret' => $socialUser->tokenSecret
+            ]);
+
+            Auth::login($newUser);
+            return response()->json($userInfo);
         } catch (Exception $e) {
             \Log::debug('SNS認証中にエラーが発生しました。'. $e->getMessage());
             \Log::debug('   ');
