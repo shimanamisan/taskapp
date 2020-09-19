2a830f7ee7f66906a1edb57760ccdfcd2b6fa59d
shimanamisan <mikan.sup.all@gmail.com>
2020-09-19 22:57:01 +0900

    トークン認証切れ時の処理も追加

--- resources/js/components/EmailChangeCallback.vue
+++ resources/js/components/EmailChangeCallback.vue
@@ -13,9 +13,7 @@
                 </p>
                 <div v-else>
                     <p class="p-callback__wrapp__text">{{ authMessage }}</p>
-                    <router-link class="c-btn u-btn__callback" to="/login"
-                        >ログインページに戻る</router-link
-                    >
+                    <p>3秒後にトップ画面へ移動します。移動しない場合はブラウザをリロードして下さい。</p>
                 </div>
             </div>
         </div>
@@ -57,35 +55,53 @@ export default {
         }),
         async emailChangeAuth() {
             this.authMessage = "認証用トークンを確認しています。。。";
-            const response = await axios.get("/api/mypage/change-password/", {
+            const response = await axios.get("/api/mypage/change_email", {
                 params: this.$route.query
             });
+            console.log(response);
             if (response.status === OK) {
-                if (this.$route.query.denied) {
-                    this.setApiStatus(false);
-                    this.auth_flg = false;
-                    this.authMessage =
-                        "認証に失敗しました。ログインページへ戻ります。";
-                    setTimeout(() => {
-                        this.$router.push("/login");
-                    }, 2000);
-                    return;
-                } else {
-                    this.authMessage = "トークンの認証に成功しました。";
-                    this.setApiStatus(true);
-                    this.setUser(response.data.name);
-                    this.setEmail(response.data.email);
-                    this.setPic(response.data.pic);
-                    this.setId(response.data.id);
-                    setTimeout(() => {
-                        this.$router.push("/profile");
-                    }, 2000);
-                    return;
-                }
+                this.authMessage = "メールアドレスを変更しました。";
+                this.setApiStatus(true);
+                setTimeout(() => {
+                    this.$router.push("/profile");
+                }, 2000);
+                return;
             } else if (response.status === UNPROCESSABLE_ENTITY) {
                 // エラーメッセージを格納する
                 this.auth_flg = false;
-                this.authMessage = response.data.error_message;
+                this.authMessage = response.data.errors.email[0];
+                this.setUser(null);
+                this.setEmail(null);
+                this.setPic(null);
+                this.setId(null);
+                // バックエンド側でセッションを削除しているので、一旦リロードさせる
+                setTimeout(() => {
+                    window.location = "/";
+                }, 3000);
+                return;
+            } else if (response.status === INTERNAL_SERVER_ERROR) {
+                this.auth_flg = false;
+                this.authMessage = response.data.errors.email[0];
+                this.setUser(null);
+                this.setEmail(null);
+                this.setPic(null);
+                this.setId(null);
+                // バックエンド側でセッションを削除しているので、一旦リロードさせる
+                setTimeout(() => {
+                    window.location = "/";
+                }, 3000);
+                return;
+            } else {
+                this.auth_flg = false;
+                this.authMessage = 'トークンの有効期限が切れているか、ログインされていません。';
+                this.setUser(null);
+                this.setEmail(null);
+                this.setPic(null);
+                this.setId(null);
+                // バックエンド側でセッションを削除しているので、一旦リロードさせる
+                setTimeout(() => {
+                    window.location = "/";
+                }, 3000);
                 return;
             }
         }
