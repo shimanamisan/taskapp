1cbf36e3e9f8cddefed6cfe364ee4530546bc995
System Administrator <root@myMac.local>
2021-04-23 06:21:58 +0900

    [update] postcss-loaderを一時的に無効化

--- webpack.config.js
+++ webpack.config.js
@@ -15,11 +15,11 @@ const WebpackBuildNotifierPlugin = require("webpack-build-notifier");
 // プロダクションモードでバンドル時に、console.logを自動的に削除するプラグイン
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 // ファイルをコピーするプラグイン
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 // 各種画像形式の圧縮ツールを取りまとめる
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
 // jpgファイルを圧縮する
-const ImageminMozjpeg = require('imagemin-mozjpeg');
+const ImageminMozjpeg = require("imagemin-mozjpeg");
 
 // [定数] webpack の出力オプションを指定します
 // 'production' か 'development' を指定
@@ -45,22 +45,22 @@ module.exports = {
         // 出力ファイル名
         filename: "app.js",
         // 出力先フォルダを指定
-        path: mydir + "/public/js"
+        path: mydir + "/public/js",
     },
     // 最適化（webpack4から導入された）
     optimization: {
         minimizer: [
             new TerserPlugin({
-                extractComments: "all"
+                extractComments: "all",
             }),
             new UglifyJsPlugin({
                 uglifyOptions: {
                     compress: {
-                        drop_console: true
-                    }
-                }
+                        drop_console: true,
+                    },
+                },
             }),
-            new OptimizeCssAssetsPlugin({})
+            new OptimizeCssAssetsPlugin({}),
         ],
     },
     module: {
@@ -79,42 +79,42 @@ module.exports = {
                             // url()を変換しない
                             // url: false,
                             // ソースマップを有効にする
-                            sourceMap: enabledSourceMap
-                        }
+                            sourceMap: enabledSourceMap,
+                        },
                     },
                     {
                         // Sassをバンドルするための機能
                         loader: "sass-loader",
                         options: {
                             // ソースマップの利用有無
-                            sourceMap: enabledSourceMap
-                        }
+                            sourceMap: enabledSourceMap,
+                        },
                     },
                     {
-                        loader: "import-glob-loader"
+                        loader: "import-glob-loader",
                     },
                     // PostCSSのための設定★
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            // PostCSS側でもソースマップを有効にする
-                            sourceMap: true,
-                            plugins: [
-                                // Autoprefixerを有効化
-                                require("autoprefixer")({
-                                    // ☆IEは11以上、Androidは4.4以上
-                                    // その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
-                                    browsers: [
-                                        "last 2 versions",
-                                        "ie >= 11",
-                                        "Android >= 4",
-                                        "iOS >= 8"
-                                    ]
-                                })
-                            ]
-                        }
-                    }
-                ]
+                    // {
+                    //     loader: "postcss-loader",
+                    //     options: {
+                    //         // PostCSS側でもソースマップを有効にする
+                    //         sourceMap: true,
+                    //         plugins: [
+                    //             // Autoprefixerを有効化
+                    //             require("autoprefixer")({
+                    //                 // ☆IEは11以上、Androidは4.4以上
+                    //                 // その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
+                    //                 browsers: [
+                    //                     "last 2 versions",
+                    //                     "ie >= 11",
+                    //                     "Android >= 4",
+                    //                     "iOS >= 8"
+                    //                 ]
+                    //             })
+                    //         ]
+                    //     }
+                    // }
+                ],
             },
             {
                 // Vueファイルに対する設定
@@ -124,17 +124,17 @@ module.exports = {
                         loader: "vue-loader",
                         options: {
                             loaders: {
-                                js: "babel-loader"
+                                js: "babel-loader",
                             },
                             options: {
                                 presets: [
                                     // プリセットを指定することで、ES2020 を ES5 に変換
-                                    "@babel/preset-env"
-                                ]
-                            }
-                        }
-                    }
-                ]
+                                    "@babel/preset-env",
+                                ],
+                            },
+                        },
+                    },
+                ],
             },
             {
                 // JSファイルに対する設定
@@ -145,11 +145,11 @@ module.exports = {
                         options: {
                             presets: [
                                 // プリセットを指定することで、ES2020 を ES5 に変換
-                                "@babel/preset-env"
-                            ]
-                        }
-                    }
-                ]
+                                "@babel/preset-env",
+                            ],
+                        },
+                    },
+                ],
             },
             {
                 // 拡張子の大文字も許容するように最後尾に i を加える
@@ -161,10 +161,10 @@ module.exports = {
                     // 画像のパスは別から取得できるので、指定のサイズを超えるものが複数ある場合はそれらをブラウザが並行して取得する
                     limit: 2048,
                     // jsディレクトリから見た相対パス
-                    name: "../img/[name].[ext]"
-                }
-            }
-        ]
+                    name: "../img/[name].[ext]",
+                },
+            },
+        ],
     },
     // 各種プラグインを読み込む
     plugins: [
@@ -175,43 +175,42 @@ module.exports = {
         // jsファイルとcssファイルを分割するためのプラグイン
         new MiniCssExtractPlugin({
             // ファイルの出力先。エントリーポイントのjsディレクトリが基準となるので出力先には注意
-            filename: "../css/style.css"
+            filename: "../css/style.css",
         }),
         // ビルド時に通知するためのプラグイン
         new WebpackBuildNotifierPlugin({
-            suppressSuccess: true
+            suppressSuccess: true,
         }),
         // ファイルをコピーするプラグイン
         new CopyPlugin({
             patterns: [
-              {
-                from: path.resolve(__dirname, 'resources/img/'),
-                to: path.resolve(__dirname, 'public/img/'),
-                // from: "./resources/img/*",
-                // to: "@/../../public/img", // ディレクトリ構成もコピーされる
-                // context: ".src/sample",
-              },
+                {
+                    from: path.resolve(__dirname, "resources/img/"),
+                    to: path.resolve(__dirname, "public/img/"),
+                    // from: "./resources/img/*",
+                    // to: "@/../../public/img", // ディレクトリ構成もコピーされる
+                    // context: ".src/sample",
+                },
             ],
-          }),
-          new ImageminPlugin({
+        }),
+        new ImageminPlugin({
             test: /\.(jpe?g|jpg|png|gif|svg)$/i,
             pngquant: {
-              quality: '50'
+                quality: "50",
             },
             gifsicle: {
-              interlaced: false,
-              optimizationLevel: 1,
-              colors: 256
+                interlaced: false,
+                optimizationLevel: 1,
+                colors: 256,
             },
-            svgo: {
-            },
+            svgo: {},
             plugins: [
-              ImageminMozjpeg({
-                quality: 60,
-                progressive: true
-              }),
-            ]
-          })
+                ImageminMozjpeg({
+                    quality: 60,
+                    progressive: true,
+                }),
+            ],
+        }),
     ],
     // import 文で .ts ファイルを解決するため
     resolve: {
@@ -219,8 +218,8 @@ module.exports = {
         alias: {
             vue$: "vue/dist/vue.esm.js",
             // jsディレクトリまでのフルパスをエイリアスとして設定
-            '@': path.resolve(__dirname, 'resources/js/'),
+            "@": path.resolve(__dirname, "resources/js/"),
         },
-        extensions: ["*", ".js", ".vue", ".json"]
-    }
+        extensions: ["*", ".js", ".vue", ".json"],
+    },
 };
